c1c1de760a16d850f9df14ae5228c83d4d0860c2
JhelkyWalliya <jhelky18411061@yahoo.com>
2022-06-16 03:58:07 -0700

    final02

---
+++ index.php
@@ -0,0 +1,25 @@
+<a href="create.php"> create</a>
+
+<hr>
+<?php
+
+
+
+if (isset($_GET['nama_legkap'])&&isset(($email = $_GET['email']);{
+
+        // echo "hello world"
+        $nama_lengkap = $_GET['nama_legkap'];
+        $email = $_GET['email'];
+    
+    
+        
+        // menampilkan data
+        echo $nama_lengkap;
+        echo '<be><br>';
+        echo 'email';
+    
+
+}
+
+
+    ?>
