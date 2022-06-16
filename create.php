daa0e147e93d6c204110887a7b9dec65ee239491
JhelkyWalliya <jhelky18411061@yahoo.com>
2022-06-16 03:56:37 -0700

    finalo2

---
+++ create.php
@@ -0,0 +1,11 @@
+<form action="index.php" method="GET">
+
+    <hr>
+
+    <h3>Form create</h3>
+
+    <input type="text" "nama_legkap" placeholder="nama lengkap"> <br>
+    <input type="text" "Alamat_email" placeholder="Alamat Email"> <br>
+    <input type="submit" value="kirim" placeholder=""> <br>
+
+</form>
